8d3682e95440a71fea7b620e980e4e8e4732e2a3
Therese Elvira Mombou Gatsing <tmombougatsing@mail.wlu.edu>
2026-04-21 16:19:51 -0400

    implementing a newsletter

---
+++ country.java
@@ -0,0 +1,14 @@
+public class Country {
+
+    private String country;
+
+    //constructor
+    public Country(String country){
+
+        this.country = country;
+    }
+
+    public static void main(String[] args){
+        
+    }
+}
--- main.java
+++ main.java
@@ -4,7 +4,7 @@ public class main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a country of your choice: ");
-        scanner.nextLine();
+        String country = scanner.nextLine();
         scanner.close();
     }
 }
